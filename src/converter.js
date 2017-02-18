67cb71bc6e06d5babc8b7a1ea1309d878d884da2
Raphael ATALLAH <raphael@noemail.fr>
2017-02-19 00:56:01 +0100

    Updated utils

--- src/converter.js
+++ src/converter.js
@@ -7,11 +7,29 @@ const powerOften = Math.pow(10, numberOfDecimals);
 const shuffleMatrix = [28, 9, 42, 22, 20, 32, 33, 1, 2, 29, 37, 19, 10, 23, 41, 14, 6, 40, 21, 17, 27, 4, 35, 24, 38, 3, 26, 5, 36, 7, 0, 39, 30, 11, 16, 15, 31, 34, 8, 12, 18, 13, 25];
 
 export function truncate(number) {
-    return Math.floor(number * powerOften) / powerOften
+    return Number(number.toFixed(numberOfDecimals));
 }
 
 export function padWithZeros(number) {
     return _.padStart(String(number), numberOfDecimals + 1 + numberOfIntegerNumbers, '0'); // 4 decimals + dot + integer part
+}
+
+export function setLatInRange(input) {
+    return truncate(input + 180) * powerOften;
+}
+export function setLngInRange(input) {
+    return truncate(input + 90) * powerOften;
+}
+
+export function unsetLatInRange(input) {
+    return truncate((input) / powerOften - 180);
+}
+export function unsetLngInRange(input) {
+    return truncate(input / powerOften - 90);
+}
+
+function setLength(input, length) {
+    return _.padStart(_.trimStart(input, '0'), length, '0');
 }
 
 export function combineAsBinary(lat, lng) {
@@ -20,8 +38,8 @@ export function combineAsBinary(lat, lng) {
     const latAsBinary = latInteger.toString(2);
     const lngAsBinary = lngInteger.toString(2);
 
-    const latCorrectLength = _.padStart(_.trimStart(latAsBinary, '0'), 22, '0');
-    const lngCorrectLength = _.padStart(_.trimStart(lngAsBinary, '0'), 21, '0');
+    const latCorrectLength = setLength(latAsBinary, 22);
+    const lngCorrectLength = setLength(lngAsBinary, 21);
 
     return latCorrectLength + lngCorrectLength;
 }
@@ -63,4 +81,29 @@ export function split(input) {
 
 export function unsplit(inputs) {
     return inputs.join('');
+}
+
+export function getThreeNumbersFromLatLng(lat, lng) {
+    const latInRange = setLatInRange(lat);
+    const lngInRange = setLngInRange(lng);
+
+    const binary = combineAsBinary(latInRange, lngInRange);
+    console.log(binary);
+    const shuffled = shuffle(binary);
+    const splitted = split(shuffled);
+
+    return splitted.map(i => parseInt(i, 2));
+}
+
+export function getLatLngFromThreeNumbers(numbers) {
+    const shuffled = unsplit(numbers.map(i => setLength(i.toString(2), i == 2 ? 13 : 15)));
+    const binary = unshuffle(shuffled);
+    console.log(binary);
+
+    const {lat, lng} = binaryFromLatitudeLongitude(binary);
+
+    return {
+        lat: unsetLatInRange(lat),
+        lng: unsetLatInRange(lng),
+    }
 }
--- test/converter.js
+++ test/converter.js
@@ -6,25 +6,56 @@ import {
   split,
   unsplit,
   combineAsBinary,
-  binaryFromLatitudeLongitude
+  binaryFromLatitudeLongitude,
+  setLatInRange,
+  setLngInRange,
+  unsetLatInRange,
+  unsetLngInRange,
+  getThreeNumbersFromLatLng,
+  getLatLngFromThreeNumbers
 } from '../src/converter.js';
 import { expect } from 'chai';
 
-const carnoux = {
+const carnoux1 = {
   lat: 43.258996,
   lng: 5.565438
+}
+
+const carnoux2 = {
+  lat: 43.258783,
+  lng: 5.564971
 }
 
 describe('Converter', () => {
 
   it('truncate should limit the number of decimals', () => {
-    const result = truncate(carnoux.lat);
-    expect(result).to.equal(43.2589);
+    const result = truncate(carnoux1.lat);
+    expect(result).to.equal(43.259);
   });
 
   it('should pad a number with zeros', () => {
     const result = padWithZeros(43.2589);
     expect(result).to.equal('043.2589');
+  });
+
+  it('should set a latitude between 0 and 3600000', () => {
+    const result = setLatInRange(43.2589);
+    expect(result).to.equal(2232589);
+  });
+
+  it('should set a longitude between 0 and 1800000', () => {
+    const result = setLngInRange(5.565438);
+    expect(result).to.equal(955654);
+  });
+
+  it('should restore a latitude', () => {
+    const result = unsetLatInRange(2232589);
+    expect(result).to.equal(43.2589);
+  });
+
+  it('should restore a longitude', () => {
+    const result = unsetLngInRange(955654);
+    expect(result).to.equal(5.5654);
   });
 
   it('should create a binary version of the latitude and longitude', () => {
@@ -57,4 +88,23 @@ describe('Converter', () => {
     const results = unsplit(['100101110100110', '110000100011011', '1011000111001']);
     expect(results).to.equal('1001011101001101100001000110111011000111001');
   });
+
+  it('should generate three numbers from lat lng', () => {
+    const result = getThreeNumbersFromLatLng(carnoux1.lat, carnoux1.lng);
+    expect(result).to.deep.equal([26721, 32388, 6787]);
+  });
+
+ xit('should generate three different numbers from different lat lng', () => {
+    const result = getThreeNumbersFromLatLng(carnoux2.lat, carnoux2.lng);
+    expect(result).to.deep.equal([25673, 31428, 6790]);
+  });
+
+  it('should get lat lng back from three numbers', () => {
+    const result = getLatLngFromThreeNumbers([26721, 32388, 6787]);
+    expect(result).to.deep.equal({
+      lat: carnoux2.lat,
+      lng: carnoux2.lng
+    });
+  });
+
 });
